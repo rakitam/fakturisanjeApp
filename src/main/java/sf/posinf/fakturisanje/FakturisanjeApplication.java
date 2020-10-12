b86f2b92daef30057d5fe2c282dbb0f460d29c88
unknown <mrakita1993@gmail.com>
2020-10-12 23:20:10 +0200

    Inicijalizacija projekta

---
+++ src/main/java/sf/posinf/fakturisanje/FakturisanjeApplication.java
@@ -0,0 +1,13 @@
+package sf.posinf.fakturisanje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakturisanjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakturisanjeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sf/posinf/fakturisanje/FakturisanjeApplicationTests.java
@@ -0,0 +1,13 @@
+package sf.posinf.fakturisanje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakturisanjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
