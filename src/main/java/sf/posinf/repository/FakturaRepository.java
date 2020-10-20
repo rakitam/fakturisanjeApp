adb337f8a6126bac1b418a20d9940ec2773bef31
unknown <mrakita1993@gmail.com>
2020-10-20 19:11:36 +0200

    Dodate FakturaRepository i FakturaServiceInterface klase

---
+++ src/main/java/sf/posinf/repository/FakturaRepository.java
@@ -0,0 +1,28 @@
+package sf.posinf.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import sf.posinf.model.Faktura;
+
+@Repository
+public interface FakturaRepository extends JpaRepository<Faktura, Long> {
+
+	List<Faktura> findAllByVrstaFakture(boolean vrstaFakture);
+	
+	List<Faktura> findAllByVrstaFaktureAndPoslovniPartner_NazivPartnera(boolean vrstaFakture, String nazivPartnera);
+	
+	List<Faktura> findAllByVrstaFaktureAndPoslovniPartner_NazivPartneraAndPoslovnaGodina_Id(boolean vrstaFakture, String nazivPartnera, long poslovna_godina_id);
+	
+	List<Faktura> findAllByVrstaFaktureAndPoslovnaGodina_Id(boolean vrstaFakture, long poslovnaGodina_id);
+
+	List<Faktura> findAllByPoslovniPartner_NazivPartnera(String nazivPartnera);
+	
+	List<Faktura> findAllByPoslovnaGodina_Id(long poslovnaGodina_id);
+	
+	//Proveriti da li sam dobro namapirala enum u bazi
+	List<Faktura> findAllByStatusFakture(String statusFakture);
+}
+
---
+++ src/main/java/sf/posinf/services/interfaces/FakturaServiceInterface.java
@@ -0,0 +1,33 @@
+package sf.posinf.services.interfaces;
+
+import java.util.List;
+
+import sf.posinf.model.Faktura;
+
+//TODO: Za sad dodato vise metoda koje bi se potencijalno mogle koristiti; posle pobrisati one koje ne budu bili potrebne
+public interface FakturaServiceInterface {
+
+	List<Faktura> findAll();
+	
+	Faktura findOne(Long id);
+	
+    List<Faktura> findAllByVrstaFakture(boolean vrstaFakture);
+    
+    List<Faktura> findAllByVrstaFaktureAndPoslovnaGodina(boolean vrstaFakture, long poslovnaGodina);
+    
+    List<Faktura> findAllByPoslovniPartner(String nazivPartnera);
+    
+    List<Faktura> findAllByVrstaFaktureAndPoslovniPartner(boolean vrstaFakture, String poslovniPartner);
+    
+    List<Faktura> findAllByVrstaFaktureAndPoslovniPartnerAndPoslovnaGodina(boolean vrstaFakture, String poslovniPartner, long poslovnaGodina);
+    
+    List<Faktura> findAllByPoslovnaGodina(long poslovnaGodina);
+    
+    List<Faktura> findAllByStatusFakture(String statusFakture);
+    
+    Faktura save(Faktura faktura);
+    
+    Boolean delete(Long id);
+    
+    void update(Faktura faktura);
+}
