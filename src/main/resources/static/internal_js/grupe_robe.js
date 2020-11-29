f6a81f14ba5424032c3b2e5caf6af2a45735a813
romeomtb <roman.aleksandar76@gmail.com>
2020-11-29 17:43:00 +0100

    popravka ispisa grupe robe

--- src/main/resources/static/internal_js/grupe_robe.js
+++ src/main/resources/static/internal_js/grupe_robe.js
@@ -3,7 +3,22 @@ $(document).ready(function(){
     var grupaRobeTable = $('#grupeRobeTable');
     var pagination = $('#pagination');
     var page = 0;
+    var grupe = [];
     getGrupaRobe();
+
+    $("#add-grupa-robe").load("dialog/dodavanje_grupe_robe.html", function () {
+        $.ajax({
+            url: '/api/pdv',
+            success: function (data) {
+                var selectPDV = $('#pdv');
+                grupe = data;
+                for (var i=0; i<data.length; i++) {
+                    selectPDV.append(`<option value="${i}">${data[i].nazivPDV}</option>`);
+                }
+            }
+        });
+    });
+
 
     function getGrupaRobe() {
 
@@ -41,6 +56,51 @@ $(document).ready(function(){
         event.preventDefault();
         page = $(this).attr("page");
         getGrupaRobe();
+    });
+
+
+    $('#modalDodavanje').click(function (e) {
+        e.preventDefault();
+        $('#dodavanje_grupe_robe').modal('show');
+
+        $('#naziv').val('');
+        $('#pdv>option:eq(0)').prop('selected', true);
+    });
+
+
+    $(document).on('click', '#potvrda_dodavanja_grupe_robe', function () {
+        $('.alert').alert('close')
+        var naziv = $('#naziv').val();
+        if (naziv.length < 3) {
+            $('#messages').append(
+                `<div class=" alert alert-danger alert-dismissible fade show" role="alert">Naziv mora biti duzi od 3 slova</div>`);
+            setTimeout(function () {$('.alert').alert('close')}, 3000);
+            return;
+        }
+        var pdv = $('#pdv').val();
+        if (pdv == null) {
+            $('#messages').append(
+                `<div class=" alert alert-danger alert-dismissible fade show" role="alert">Grupa robe nije selektovana</div>`);
+            setTimeout(function () {$('.alert').alert('close')}, 3000);
+            return;
+        }
+
+        var novaGrupa = {
+            nazivGrupe: naziv,
+            pdv: grupe[pdv]
+        };
+
+        $.ajax({
+            url: '/api/grupe-robe',
+            type: 'POST',
+            data: JSON.stringify(novaGrupa),
+            contentType:"application/json",
+            success: function(data) {
+                getGrupaRobe();
+                console.log('uspesno dodata grupa');
+            }
+        });
+        $('#dodavanje_grupe_robe').modal('hide');
     });
 
 });
