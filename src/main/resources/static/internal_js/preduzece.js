7cfc970845cf7a3ddb9b10da801293b5c6a3df83
unknown <kovacevicnemanja97@gmail.com>
2020-12-03 04:11:51 +0100

    Uradjena izmena preduzeca.

--- src/main/resources/static/internal_js/preduzece.js
+++ src/main/resources/static/internal_js/preduzece.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     $('#nav').load('nav.html');
     getPreduzece();
 
+     var preduzece = {}
+
     function getPreduzece(){
         $.ajax({
             url: '/api/preduzeca/1',
@@ -15,7 +17,35 @@ $(document).ready(function () {
                 $("#telefon").val(data.telefon);
                 $("#email").val(data.email);
                 $("#PIB").val(data.pib);
+
+                preduzece = data;
             }
         });
+
     }
+
+
+
+    $('#izmeniPodatke').on("click", function(){
+
+        preduzece.naziv =  $("#naziv").val();
+        preduzece.adresaPreduzeca =  $("#adresaPreduzeca").val();
+        preduzece.mesto.id =  $("#mesto").val();
+        preduzece.telefonPreduzeca =  $("#telefonPreduzeca").val();
+        preduzece.tekuciRacun =  $("#tekuciRacun").val();
+        preduzece.telefon =  $("#telefon").val();
+        preduzece.email =  $("#email").val();
+        preduzece.PIB =  $("#PIB").val();
+
+        $.ajax({
+            method: 'PUT',
+            data: JSON.stringify(preduzece),
+            contentType: "application/json",
+            headers: {"Authorization": localStorage.getItem('token')},
+            url: '/api/preduzeca/1'
+        }).done(function(){
+            console.log(preduzece)
+            getPreduzece()
+        });
+    });
 });
